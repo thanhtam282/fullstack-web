863930af74a6e2031c4feba8c6480659075e39a0
Nguyen Thanh Tam <ntt.hcm.vn@gmail.com>
2018-09-15 19:18:30 +0700

    upload code

---
+++ src/scripts/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$(".thanhtam-slider-top .owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true
+	});
+	$(".thanhtam-client .owl-carousel").owlCarousel({
+		items: 1,
+		dots: true,
+		//nav: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true
+		
+	});
+	$(".home-slider .owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true,
+		responsive: {
+			// breakpoint from 480 up
+			360: {
+				items: 2,
+			},
+			480: {
+				items: 2,
+			},
+			640: {
+				items: 3,
+			},
+			// breakpoint from 768 up
+			768: {
+				items: 4,
+			},
+			// breakpoint from 992 up
+			992: {
+				items: 6,
+			}
+		}
+	});
+});
